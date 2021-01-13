1dbe6d0a7e070f9142c6838c5280f4148ed38bc8
Mikhail Sidorov <7780462+mikhailsidorov@users.noreply.github.com>
2021-01-13 07:06:09 +0300

    Fix fetching issue

--- src/setupProxy.ts
+++
@@ -1,6 +0,0 @@
-import proxy from 'http-proxy-middleware';
-import { Application } from 'express';
-
-module.exports = function(app:Application) {
-  app.use(proxy('/opendata/', { target: 'https://data.gov.ru/sites/default/files', changeOrigin: true, secure: false }));
-};
